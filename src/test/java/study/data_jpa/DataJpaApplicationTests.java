169bb2ab2c3d3c8cc70f0c05a103b2e2ae583b55
zwoong <zwoong6867@gmail.com>
2025-06-09 21:27:28 +0900

    ✨ 프로젝트 세팅

---
+++ src/test/java/study/data_jpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
