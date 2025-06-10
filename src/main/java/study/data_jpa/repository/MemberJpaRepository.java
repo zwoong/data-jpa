4db694359349a2580a80e8052c87a6744773ceea
zwoong <zwoong6867@gmail.com>
2025-06-10 21:57:54 +0900

    ✨ 순수 JPA 기반 리포지토리 만들기

--- src/main/java/study/data_jpa/repository/MemberJpaRepository.java
+++ src/main/java/study/data_jpa/repository/MemberJpaRepository.java
@@ -2,6 +2,8 @@ package study.data_jpa.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import study.data_jpa.entity.Member;
 
@@ -14,6 +16,25 @@ public class MemberJpaRepository {
   public Member save(Member member) {
     em.persist(member);
     return member;
+  }
+
+  public void delete(Member member) {
+    em.remove(member);
+  }
+
+  public List<Member> findAll() {
+    return em.createQuery("select m from Member m", Member.class)
+        .getResultList();
+  }
+
+  public Optional<Member> findById(Long id) {
+    Member member = em.find(Member.class, id);
+    return Optional.ofNullable(member);
+  }
+
+  public long count() {
+    return em.createQuery("select count(m) from Member m", Long.class)
+        .getSingleResult();
   }
 
   public Member find(Long id) {
