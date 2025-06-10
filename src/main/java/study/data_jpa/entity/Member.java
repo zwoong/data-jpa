2e467b5b27b31b1deb6826ac9a6523dc95b747e4
zwoong <zwoong6867@gmail.com>
2025-06-10 20:12:11 +0900

    ✨ 예제 도메인 모델과 동작확인

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -1,22 +1,46 @@
 package study.data_jpa.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
   @Id @GeneratedValue
+  @Column(name = "member_id")
   private Long id;
   private String username;
+  private int age;
 
-  protected Member() {
-  }
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "team_id")
+  private Team team;
 
   public Member(String username) {
     this.username = username;
+  }
+
+  public Member(String username, int age, Team team) {
+    this.username = username;
+    this.age = age;
+    if (team != null) {
+      changeTeam(team);
+    }
+  }
+
+  public void changeTeam(Team team) {
+    this.team = team;
+    team.getMembers().add(this);
   }
 }
