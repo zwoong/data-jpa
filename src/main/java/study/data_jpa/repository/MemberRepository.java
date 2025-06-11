da6847a06629918f20879c51b9b55c8add469b30
zwoong <zwoong6867@gmail.com>
2025-06-11 21:27:25 +0900

    ✨ 파라미터 바인딩

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -21,4 +21,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
   @Query("select new study.data_jpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
   List<MemberDto> findMemberDto();
+
+  @Query("select m from Member m where m.username in :names")
+  List<Member> findMyNames(@Param("names") List<String> names);
 }
--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package study.data_jpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -122,6 +123,19 @@ class MemberRepositoryTest {
     List<MemberDto> memberDto = memberRepository.findMemberDto();
     for (MemberDto dto : memberDto) {
       System.out.println("dto = " + dto);
+    }
+  }
+
+  @Test
+  public void findMyNames() {
+    Member m1 = new Member("AAA", 10);
+    Member m2 = new Member("BBB", 20);
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    List<Member> result = memberRepository.findMyNames(Arrays.asList("AAA", "BBB"));
+    for (Member member : result) {
+      System.out.println("member = " + member);
     }
   }
 }
