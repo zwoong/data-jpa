9ca02ece552f5b6242a13cad34024f9c8f8ae7fc
zwoong <zwoong6867@gmail.com>
2025-06-16 22:17:02 +0900

    ✨ @EntityGraph

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -39,4 +40,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   @Modifying(clearAutomatically = true)
   @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
   int bulkAgePlus(@Param("age") int age);
+
+  @Query("select m from Member m left join fetch m.team")
+  List<Member> findMemberFetchJoin();
+
+  @Override
+  @EntityGraph(attributePaths = {"team"})
+  List<Member> findAll();
 }
