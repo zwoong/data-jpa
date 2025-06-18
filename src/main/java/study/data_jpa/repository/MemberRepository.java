9d30e4699d9dc137804f96504482d07240b0ff68
zwoong <zwoong6867@gmail.com>
2025-06-18 21:07:17 +0900

    ✨ JPA Hint & Lock

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package study.data_jpa.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
@@ -7,8 +9,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
@@ -47,4 +51,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   @Override
   @EntityGraph(attributePaths = {"team"})
   List<Member> findAll();
+
+  @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+  Member findReadOnlyByUsername(String username);
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  List<Member> findLockByUsername(String username);
 }
--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -234,4 +234,32 @@ class MemberRepositoryTest {
       System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
     }
   }
+
+  @Test
+  public void queryHint() {
+    Member member1 = new Member("member1", 10);
+
+    memberRepository.save(member1);
+    em.flush();
+    em.clear();
+
+    Member findMember = memberRepository.findReadOnlyByUsername("member1");
+    findMember.setUsername("member2");
+
+    em.flush();
+  }
+
+  @Test
+  public void lock() {
+    Member member1 = new Member("member1", 10);
+
+    memberRepository.save(member1);
+    em.flush();
+    em.clear();
+
+    List<Member> findMember = memberRepository.findLockByUsername("member1");
+
+    em.flush();
+  }
+
 }
