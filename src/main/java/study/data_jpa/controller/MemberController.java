cd3f326c38a70e50a90767f17c41bbbab7535db8
zwoong <zwoong6867@gmail.com>
2025-06-18 22:20:43 +0900

    ✨ 도메인 클래스 컨버터

---
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -0,0 +1,32 @@
+package study.data_jpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.entity.Member;
+import study.data_jpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+  private final MemberRepository memberRepository;
+
+  @GetMapping("/members/{id}")
+  public String findMember(@PathVariable("id") Long id) {
+    Member member = memberRepository.findById(id).get();
+    return member.getUsername();
+  }
+
+  @GetMapping("/members/{id}")
+  public String findMember2(@PathVariable("id") Member member) {
+    return member.getUsername();
+  }
+
+  @PostConstruct
+  public void init() {
+    memberRepository.save(new Member("userA"));
+  }
+}
