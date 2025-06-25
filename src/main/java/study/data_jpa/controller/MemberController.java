9f4855e57ce1eac6ca6843da768f05df95833e19
zwoong <zwoong6867@gmail.com>
2025-06-25 21:40:12 +0900

    ✨ Web 확장 - 페이징과 정렬

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,6 +2,8 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,19 +16,26 @@ public class MemberController {
 
   private final MemberRepository memberRepository;
 
-  @GetMapping("/members/{id}")
-  public String findMember(@PathVariable("id") Long id) {
-    Member member = memberRepository.findById(id).get();
-    return member.getUsername();
-  }
+//  @GetMapping("/members/{id}")
+//  public String findMember(@PathVariable("id") Long id) {
+//    Member member = memberRepository.findById(id).get();
+//    return member.getUsername();
+//  }
 
   @GetMapping("/members/{id}")
   public String findMember2(@PathVariable("id") Member member) {
     return member.getUsername();
   }
 
+  @GetMapping("/members")
+  public Page<Member> list(Pageable pageable) {
+    return memberRepository.findAll(pageable);
+  }
+
   @PostConstruct
   public void init() {
-    memberRepository.save(new Member("userA"));
+    for (int i = 0; i < 100; i++) {
+      memberRepository.save(new Member("user" + i, i));
+    }
   }
 }
--- src/main/java/study/data_jpa/dto/MemberDto.java
+++ src/main/java/study/data_jpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.data_jpa.dto;
 
 import lombok.Data;
+import study.data_jpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -13,5 +14,11 @@ public class MemberDto {
     this.id = id;
     this.username = username;
     this.teamName = teamName;
+  }
+
+  public MemberDto(Member member) {
+    this.id = member.getId();
+    this.username = member.getUsername();
+    this.teamName = member.getTeam().getName();
   }
 }
